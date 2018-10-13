0c0f84b70abd43c9f13af7a47a39d8281a75e4e2
Ronen Sabag <sabag.ronen@gmail.com>
2018-10-13 13:54:51 +0300

    add cfp logic in the presenter (#8)

--- app/src/main/java/io/sabag/androidConferences/conferencesList/ConferencesListPresenter.kt
+++ app/src/main/java/io/sabag/androidConferences/conferencesList/ConferencesListPresenter.kt
@@ -3,6 +3,8 @@ package io.sabag.androidConferences.conferencesList
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.MutableLiveData
 import io.sabag.androidConferences.ConferenceState
+import io.sabag.androidConferences.ExtraInfo
+import io.sabag.androidConferences.R
 import io.sabag.androidConferences.pluginInterfaces.ITimeAndDateUtils
 
 class ConferencesListPresenter(
@@ -20,10 +22,17 @@ class ConferencesListPresenter(
     override fun loadData() {
         interActor.observeConferences{ conferencesDetailsList ->
             val list = conferencesDetailsList.map {
+                val extraInfo = when (it.cfpStatus) {
+                    CfpStatus.CFP_STATUS_NOT_STARTED -> ExtraInfo("cfp", R.drawable.status_not_started)
+                    CfpStatus.CFP_STATUS_IN_PROGRESS -> ExtraInfo("cfp", R.drawable.status_in_progress)
+                    CfpStatus.CFP_STATUS_ENDED -> ExtraInfo("cfp", R.drawable.status_ended)
+                    CfpStatus.CFP_STATUS_NA -> null
+                }
                 ConferenceState(
                         title = it.name,
                         location = it.location,
-                        info = getDates(it)
+                        info = getDates(it),
+                        extraInfo = extraInfo
                 )
             }
             conferencesStateLiveData.postValue(list)
