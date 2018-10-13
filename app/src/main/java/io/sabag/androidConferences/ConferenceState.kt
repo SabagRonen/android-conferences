ae5f9347ed002bec391b745d0bc94c25ac69d749
ronen <sabag.ronen@gmail.com>
2018-10-13 10:57:36 +0300

    add status info UI that reflect the status of CFP

--- app/src/main/java/io/sabag/androidConferences/ConferenceState.kt
+++ app/src/main/java/io/sabag/androidConferences/ConferenceState.kt
@@ -1,7 +1,15 @@
 package io.sabag.androidConferences
 
+import androidx.annotation.DrawableRes
+
+data class ExtraInfo (
+        val text: String,
+        @DrawableRes val icon: Int? = null
+)
+
 data class ConferenceState(
         val title: String,
         val location: String,
-        val info: String
+        val info: String,
+        val extraInfo: ExtraInfo? = null
 )
